16b2999e33403d85e50dcfb68fedf6d8b1a8ded4
Yasu3D <l.frisan31@gmail.com>
2024-03-05 18:14:29 +0100

    there's always a way to make it work

--- Views/Settings/SettingsView_Keymap.axaml.cs
+++ Views/Settings/SettingsView_Keymap.axaml.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.VisualTree;
+using Avalonia.LogicalTree;
 using MercuryMapper.Config;
 
 namespace MercuryMapper.Views.Settings;
@@ -22,41 +22,11 @@ public partial class SettingsView_Keymap : UserControl
 
     private void SetKeybindTags()
     {
-        // This doesn't work because Avalonia is stupid.
-        //foreach (TreeViewItem item in KeybindsTreeView.GetVisualDescendants().OfType<TreeViewItem>())
-        //    item.Tag = Keymap.Keybinds[item.Name!].ToString();
-        
-        FileNew.Tag = Keymap.Keybinds["FileNew"].ToString();
-        FileOpen.Tag = Keymap.Keybinds["FileOpen"].ToString();
-        FileSave.Tag = Keymap.Keybinds["FileSave"].ToString();
-        FileSaveAs.Tag = Keymap.Keybinds["FileSaveAs"].ToString();
-        FileSettings.Tag = Keymap.Keybinds["FileSettings"].ToString();
-        EditUndo.Tag = Keymap.Keybinds["EditUndo"].ToString();
-        EditRedo.Tag = Keymap.Keybinds["EditRedo"].ToString();
-        EditCut.Tag = Keymap.Keybinds["EditCut"].ToString();
-        EditCopy.Tag = Keymap.Keybinds["EditCopy"].ToString();
-        EditPaste.Tag = Keymap.Keybinds["EditPaste"].ToString();
-        EditorInsert.Tag = Keymap.Keybinds["EditorInsert"].ToString();
-        EditorPlay.Tag = Keymap.Keybinds["EditorPlay"].ToString();
-        EditorIncreasePlaybackSpeed.Tag = Keymap.Keybinds["EditorIncreasePlaybackSpeed"].ToString();
-        EditorDecreasePlaybackSpeed.Tag = Keymap.Keybinds["EditorDecreasePlaybackSpeed"].ToString();
-        EditorNoteTypeTouch.Tag = Keymap.Keybinds["EditorNoteTypeTouch"].ToString();
-        EditorNoteTypeSlideClockwise.Tag = Keymap.Keybinds["EditorNoteTypeSlideClockwise"].ToString();
-        EditorNoteTypeSlideCounterclockwise.Tag = Keymap.Keybinds["EditorNoteTypeSlideCounterclockwise"].ToString();
-        EditorNoteTypeSnapForward.Tag = Keymap.Keybinds["EditorNoteTypeSnapForward"].ToString();
-        EditorNoteTypeSnapBackward.Tag = Keymap.Keybinds["EditorNoteTypeSnapBackward"].ToString();
-        EditorNoteTypeChain.Tag = Keymap.Keybinds["EditorNoteTypeChain"].ToString();
-        EditorNoteTypeHold.Tag = Keymap.Keybinds["EditorNoteTypeHold"].ToString();
-        EditorNoteTypeMaskAdd.Tag = Keymap.Keybinds["EditorNoteTypeMaskAdd"].ToString();
-        EditorNoteTypeMaskRemove.Tag = Keymap.Keybinds["EditorNoteTypeMaskRemove"].ToString();
-        EditorNoteTypeEndOfChart.Tag = Keymap.Keybinds["EditorNoteTypeEndOfChart"].ToString();
-        EditorTypeRadio1.Tag = Keymap.Keybinds["EditorTypeRadio1"].ToString();
-        EditorTypeRadio2.Tag = Keymap.Keybinds["EditorTypeRadio2"].ToString();
-        EditorTypeRadio3.Tag = Keymap.Keybinds["EditorTypeRadio3"].ToString();
-        EditorEditNoteShape.Tag = Keymap.Keybinds["EditorEditNoteShape"].ToString();
-        EditorEditNoteProperties.Tag = Keymap.Keybinds["EditorEditNoteProperties"].ToString();
-        EditorEditNoteShapeProperties.Tag = Keymap.Keybinds["EditorEditNoteShapeProperties"].ToString();
-        EditorDelete.Tag = Keymap.Keybinds["EditorDelete"].ToString();
+        foreach (TreeViewItem item in KeybindsTreeView.GetLogicalDescendants().OfType<TreeViewItem>())
+        {
+            if (!Keymap.Keybinds.ContainsKey(item.Name ?? "")) continue;
+            item.Tag = Keymap.Keybinds[item.Name ?? ""].ToString();
+        }
     }
     
     private void KeybindsTreeView_OnDoubleTapped(object? sender, TappedEventArgs e)
