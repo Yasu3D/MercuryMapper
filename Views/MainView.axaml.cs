a864ddc7c4b1a2f2c06b6e78bce43525f0cd91d7
Yasu3D <l.frisan31@gmail.com>
2024-06-16 20:23:01 +0200

    update version number

--- Views/MainView.axaml.cs
+++ Views/MainView.axaml.cs
@@ -62,7 +62,7 @@ public partial class MainView : UserControl
     }
 
     public bool CanShutdown;
-    public const string AppVersion = "v2.0.1b";
+    public const string AppVersion = "v2.1.0";
     private const string ConfigPath = "UserConfig.toml";
     
     public UserConfig UserConfig = new();
