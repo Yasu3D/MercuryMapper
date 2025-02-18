8eedb52a51cff952f365b5c2da780f0313890128
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-02-18 03:47:33 +0100

    change version number

--- Views/MainView.axaml.cs
+++ Views/MainView.axaml.cs
@@ -74,7 +74,7 @@ public partial class MainView : UserControl
     }
 
     public bool CanShutdown;
-    public const string AppVersion = "v4.0.4";
+    public const string AppVersion = "v4.0.5";
     public const string ServerVersion = "1.0.1";
     private const string ConfigPath = "UserConfig.toml";
     private const string TimeTrackerPath = "TimeTracker";
